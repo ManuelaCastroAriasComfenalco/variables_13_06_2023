a610f32d8209800273362457caf715d8494dc995
ManuelaCastroAriasComfenalco <manuelacastro1705@gmail.com>
2023-06-13 20:01:04 -0500

    Tipos de datos

--- main.js
+++ main.js
@@ -7,6 +7,36 @@ document.querySelector("body").innerHTML = "Manuela"
 var numUno, numDos;
 
 //Tipado dinamico en JS
-var string in = helado;
+var hola = helado;
+
+//Tipos de datos en JS
+
+//LOS PRIMITIVOS
+//String
+var planta = fruta;
+
+//number
+var estatura = 166;
+var decimal = 1.66;
+
+//Boolean
+var pelota = true;
+
+//undefined
+var Mosquito;
+
+//TIPOS DE OBJETO
+
+var Outfit = 
+{
+    pantalon: "negro",
+    prendas: "4",
+    atractivo: "true",
+}
+
+
+
+
+
 
 
