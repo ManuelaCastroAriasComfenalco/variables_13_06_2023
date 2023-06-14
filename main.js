8411352992b7e70f88c4eec51a952b76fbcdb25a
ManuelaCastroAriasComfenalco <manuelacastro1705@gmail.com>
2023-06-13 19:47:30 -0500

    13/06/2023

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 console.log("Manuela")
 alert("Manuela")
 document.querySelector("body").innerHTML = "Manuela"
+
+//Declarando variables
+
+var numUno, numDos;
+
+//Tipado dinamico en JS
+var string in = helado;
+
+
